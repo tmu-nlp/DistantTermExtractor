db8aefcfd7247c1548adc4ad1674f24f2d4e886a
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-01 20:06:16 +0900

    modifiy  format

--- scripts/mylogger.py
+++ scripts/mylogger.py
@@ -10,11 +10,14 @@ def get_logger(name, file_name=None, level=INFO):
     logger = getLogger(name)
     myformat1 = '%(asctime)s - %(threadName)s - %(name)s - %(levelname)s : %(message)s'  # noqa
     myformat2 = '%(name)s - %(levelname)s : %(message)s'
-    formatter = Formatter(fmt=myformat2)
+    myformat3 = '%(asctime)s - %(name)s - %(levelname)s : %(message)s'
+    date_fmt1 = '%Y-%m-%d %H:%M:%S'
+    formatter = Formatter(fmt=myformat3, datefmt=date_fmt1)
     if file_name is None:
         handler = StreamHandler()
     else:
-        handler = FileHandler(file_name, 'w')
+        # TODO: if exist, rename other
+        handler = FileHandler(file_name, 'a')
 
     # setting of logger
     logger.setLevel(level)
