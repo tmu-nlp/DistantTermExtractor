295f53b961e3c8861511800c402b995ed6cd2c9d
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-02 17:02:33 +0900

    [add] get seeds and unlabeled data

--- scripts/distant_extractor.py
+++ scripts/distant_extractor.py
@@ -1,57 +1,28 @@
 # coding:utf-8
-u"""
-Distant Supervision による用語抽出を行います。
-
-Usage:
-    test (-c <root_cat> | --category <root_cat>) [-d <depth> | --depth <depth>] [-o <output_dir> | --output <output_dir>] [-l <log_file> | --log <log_file>]
-    test -h | --help
-    test -v | --version
-
-Option:
-     -h, --help
-        Show this screen.
-     -v, --version
-        Show version.
-     -c <root_cat>, --category <root_cat>
-        ルートカテゴリ名
-     -d <depth>, --depth <depth>
-        カテゴリの深さ [default: 1]
-     -o <output_dir>, --output <output_dir>
-        取得したシードや記事本文，抽出した用語を出力するディレクトリ [default: ../data]
-     -l <log_file>, --log <log_file> [dafault:]
-        ログ出力先ファイル
-"""
-
-from docopt import docopt
-from wikipedia_extractor import WikipediaExtractor
-import mylogger
-from file_writer import FileWriter
-
-__author__ = "ryosukee"
-__version__ = "0.0.0"
-__date__ = "2015/03/23"
 
 
 class DistantExtractor():
 
     u"""
-    seedとunlabeledデータが渡された時に勝手にラベルをつけて学習するようにする
+    DistantSupervisionを使って用語を抽出するクラス.
+
+    seedとunlabeledデータがある時に勝手にラベルをつけて学習する
+    学習したらデコードして用語を抽出する
     wikipedia周りは別にやらせる
     """
 
-    def __init__(self, root_cat, depth, logger):
+    def __init__(self, root_cat, depth, logger, file_io, wiki_ex):
         self._logger = logger
+        self._file_io = file_io
+        self._wiki_extractor = wiki_ex
+ 
         self._root_cat = root_cat
         self._limit_depth = depth
         self._seeds = list()
         self._categories = [self._root_cat]
-        self._wiki_extractor = WikipediaExtractor(
-            mylogger.get_logger(
-                WikipediaExtractor.__name__,
-                mylogger.get_filename(logger),
-                mylogger.DEBUG
-            )
-        )
+
+        self._seed_dir = 'seeds'
+        self._unlabeled_dir = 'unlabeld_corpora'
 
     def extract_seed(self):
         self._logger.info('\n------extract seed------')
@@ -70,15 +41,18 @@ class DistantExtractor():
                 self._categories += new_cats
                 current_depth += 1
                 self._logger.info('depth %d' % current_depth)
+        # output
+        self._file_io.write_list(self._seeds, self._seed_dir, 'seed')
 
-    def extract_unlabeled_data(self, file_writer):
+
+    def extract_unlabeled_data(self):
         self._logger.info('\n------extract unlabeled data--------')
 
         # Wikipedia API
         for title in self._seeds:
             content = self._wiki_extractor.get_content(title)
-            file_writer.write_string(
-                content, 'unlabeled_corpora', '%s.txt' % title)
+            self._file_io.write_string(
+                content, self._unlabeled_dir, '%s.txt' % title)
 
     def pre_process(self):
         # 文分割
@@ -108,56 +82,3 @@ class DistantExtractor():
 
     def get_seeds(self):
         return self._seeds
-
-
-def get_args(dopt):
-    args = dict()
-    for key in dopt:
-        x = dopt[key]
-        if dopt[key] is None:
-            args[key] = x
-        elif isinstance(x, bool):
-            args[key] = x
-        elif x.isdigit():
-            args[key] = int(x)
-        elif isinstance(x, unicode):
-            args[key] = x.encode('utf-8')
-        else:
-            args[key] = x
-
-    return args
-
-
-def main():
-    # init args
-    args = get_args(docopt(__doc__, version=__version__))
-
-    # init logger
-    de_logger = mylogger.get_logger(
-        DistantExtractor.__name__,
-        args['--log'],
-        mylogger.DEBUG
-    )
-    wr_logger = mylogger.get_logger(
-        FileWriter.__name__,
-        args['--log'],
-        mylogger.DEBUG
-    )
-    wiki_logger = mylogger.get_logger(
-        WikipediaExtractor.__name__,
-        args['--log'],
-        mylogger.DEBUG
-    )
-
-    # init
-    writer = FileWriter(args.output_dir, wr_logger)
-    de = DistantExtractor(args.root_cat, args.depth, de_logger)
-
-    # extract from Wikipedia
-    de.extract_seed()
-    writer.write_list(de.get_seeds(), 'seeds', 'seed')
-    de.extract_unlabeled_data(writer)
-
-
-if __name__ == '__main__':
-    main()
--- scripts/main.py
+++ scripts/main.py
@@ -23,9 +23,10 @@ Option:
 """
 
 from docopt import docopt
-from wikipedia_extractor import WikipediaExtractor
 import mylogger
-from file_writer import FileWriter
+from file_io import FileIO
+from distant_extractor import DistantExtractor
+from wikipedia_extractor import WikipediaExtractor
 
 __author__ = "ryosukee"
 __version__ = "0.0.0"
@@ -39,9 +40,51 @@ class Main():
     wikipedia周りは別にやらせる
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, root_cat, depth, log_file, output_dir):
+        
+        # init logger
+        de_logger = mylogger.get_logger(
+            DistantExtractor.__name__,
+            log_file,
+            mylogger.DEBUG
+        )
+        io_logger = mylogger.get_logger(
+            FileIO.__name__,
+            log_file,
+            mylogger.DEBUG
+        )
+        wiki_logger = mylogger.get_logger(
+            WikipediaExtractor.__name__,
+            log_file,
+            mylogger.DEBUG
+        )
 
+        # init instance
+        self._file_io = FileIO(output_dir, io_logger)
+        self._wiki_ex = WikipediaExtractor(wiki_logger, self._file_io)
+        self._distant_ex = DistantExtractor(
+            root_cat, depth, de_logger, self._file_io, self._wiki_ex)
+
+    def extract(self):
+    
+        # TODO: if you get from wikipedia
+        # このへんはオプション引数とのかねあいも考える
+        # get seed
+        self._distant_ex.extract_seed()
+        # get unlabeled data
+        self._distant_ex.extract_unlabeled_data()
+
+        # pre_prosess
+            # cleaning
+            # morpheme analysis
+            # fix form
+            # add feature
+            # labeling
+        # learn crfpp
+        # decode
+        # extract fp
+        # filtering
+        # output
 
 
 def get_args(dopt):
@@ -58,27 +101,15 @@ def get_args(dopt):
             args[key] = x.encode('utf-8')
         else:
             args[key] = x
-
     return args
 
 
 def main():
     # init args
     args = get_args(docopt(__doc__, version=__version__))
-    
-    # get seed
-    # get unlabeled data
-
-    # pre_prosess
-        # morpheme analysis
-        # fix form
-        # add feature
-        # labeling
-    # learn crfpp
-    # decode
-    # extract fp
-    # filtering
-    # output
+    # extract
+    main_process = Main(args['--category'], args['--depth'], args['--log'], args['--output'])  # noqa
+    main_process.extract()
 
 
 if __name__ == '__main__':
