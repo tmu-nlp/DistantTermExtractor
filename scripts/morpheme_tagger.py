43d5362bb22224297ed74e9124512264414dea70
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-02 21:07:36 +0900

    [add] add logger

--- scripts/morpheme_tagger.py
+++ scripts/morpheme_tagger.py
@@ -6,9 +6,10 @@ import subprocess
 
 
 class MorphemeTagger():
-    def __init__(self):
-        pass
+    def __init__(self, logger):
+        self._logger = logger
 
     def parse(self, infile, outfile):
         cmd = 'mecab -d ../my_unidic -Omyform < %s > %s' % (infile, outfile)
+        self._logger.info('exec mecab: %s' % cmd)
         subprocess.call(cmd, shell=True)
