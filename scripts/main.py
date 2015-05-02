e390a0275a6e69c7a3f654fd42fc36fcfaa457c1
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-03 00:42:28 +0900

    [add] labeling

--- scripts/main.py
+++ scripts/main.py
@@ -53,19 +53,19 @@ class Main():
         # TODO: if you get from wikipedia
         # このへんはオプション引数とのかねあいも考える
         # get seed
-#        self._distant_ex.extract_seed()
+        self._distant_ex.extract_seed()
         # get unlabeled data
-#        self._distant_ex.extract_unlabeled_data()
+        self._distant_ex.extract_unlabeled_data()
 
         # pre_prosess
         # cleaning
-#        self._distant_ex.cleaning()
+        self._distant_ex.cleaning()
         # morpheme analysis
         self._distant_ex.morpheme_tagging()
-        # fix form
         # add feature
+        self._distant_ex.add_feature()
         # labeling
-        
+        self._distant_ex.labeling()        
         
         # learn crfpp
         # decode
